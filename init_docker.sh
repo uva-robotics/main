548ee0769c36a143e480d6fa6e8c60f1d8e94ea5
Philip Goto <philip.goto@gmail.com>
2018-06-06 15:31:36 +0200

    Add hashbang to init_docker

--- init_docker.sh
+++ init_docker.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Invoke docker-compose with the 'uva-robotics' project/stack name, running in the background.
 # Use 'docker ps' to view all running containers.
 docker-compose -p uva-robotics up -d
